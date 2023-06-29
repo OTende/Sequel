749cf4db9c22396122875fb44fa421fce956bb31
OTende <mezencev_e@inbox.ru>
2023-06-30 05:06:54 +0700

    add checkbox

--- app/src/main/java/com/example/sequel/adapters/PracticeAdapter.kt
+++ app/src/main/java/com/example/sequel/adapters/PracticeAdapter.kt
@@ -3,26 +3,24 @@ package com.example.sequel.adapters
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import androidx.navigation.findNavController
 import com.example.sequel.R
 import com.example.sequel.data.Practice
-import com.example.sequel.databinding.FragmentPracticeBinding
 import com.example.sequel.databinding.PracticeListItemBinding
-import com.example.sequel.ui.fragments.FragmentPracticeCompletion
+import com.example.sequel.ui.fragments.PracticeCompletionFragment
 import javax.inject.Inject
 
 class PracticeAdapter @Inject constructor() : BaseTextAdapter<Practice, PracticeListItemBinding>() {
     override fun bind(holder: BaseTextViewHolder, item: Practice) {
         holder.binding.apply {
-//            .text = item.text
             questionTv.text = item.topic
+            practiceChecked.isChecked = item.isComplete
             questionTv.setOnClickListener {
                 holder.binding.root.findNavController().navigate(
                     R.id.action_practiceFragment_to_fragmentPracticeCompletion,
                     Bundle().apply {
                         putInt(
-                            FragmentPracticeCompletion.PRACTICE_COMPLETION_KEY,
+                            PracticeCompletionFragment.PRACTICE_COMPLETION_KEY,
                             item.id
                         )
                     }
